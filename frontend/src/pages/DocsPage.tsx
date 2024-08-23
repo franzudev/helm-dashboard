a98ee78ff274cf88cc74fcebdbaf73255f786b93
Federico François <federico.francois@kiratech.it>
2024-08-23 17:30:04 +0200

    fix: refactor repo structure

--- frontend/src/pages/DocsPage.tsx
+++
@@ -1,9 +0,0 @@
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
-import openapi from "../../public/openapi.json";
-
-const DocsPage = () => {
-  return <SwaggerUI spec={openapi} />;
-};
-
-export default DocsPage;
